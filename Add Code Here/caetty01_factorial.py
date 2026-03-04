2c6ba761216ee8f6f189d196cd222500ecbae3ae
Aaditri <aaditri01@gmail.com>
2026-03-05 03:30:25 +0530

    Add factorial function implementation

---
+++ Add Code Here/caetty01_factorial.py
@@ -0,0 +1,9 @@
+# Program to find factorial of a number
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+num = int(input("Enter a number: "))
+print("Factorial:", factorial(num))
